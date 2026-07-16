692dee8da2a347b4a17ad8aed86cb86d8f146bea
NetGeoOWild <goshan397@gmail.com>
2026-07-16 18:35:17 +0300

    update game component paths

--- src/ui/Game.tsx
+++ src/ui/Game.tsx
@@ -12,35 +12,35 @@ export type Cards = {
 const initData = [
   {
     name: "card_1",
-    src: "/assets/card_1.webp",
+    src: "./assets/card_1.webp",
   },
   {
     name: "card_2",
-    src: "/assets/card_2.webp",
+    src: "./assets/card_2.webp",
   },
   {
     name: "card_3",
-    src: "/assets/card_3.webp",
+    src: "./assets/card_3.webp",
   },
   {
     name: "card_4",
-    src: "/assets/card_4.webp",
+    src: "./assets/card_4.webp",
   },
   {
     name: "card_5",
-    src: "/assets/card_5.webp",
+    src: "./assets/card_5.webp",
   },
   {
     name: "card_6",
-    src: "/assets/card_6.webp",
+    src: "./assets/card_6.webp",
   },
   {
     name: "card_7",
-    src: "/assets/card_7.webp",
+    src: "./assets/card_7.webp",
   },
   {
     name: "card_8",
-    src: "/assets/card_8.webp",
+    src: "./assets/card_8.webp",
   },
 ];
 
